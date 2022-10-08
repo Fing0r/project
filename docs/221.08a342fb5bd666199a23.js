26b3e22782ee5ecd37aa9113b25f9b37987ac51b
Fin <fantomma91@gmail.com>
2022-10-09 00:38:21 +0700

    change docs

---
+++ docs/221.08a342fb5bd666199a23.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkproject=self.webpackChunkproject||[]).push([[221],{221:(e,t,r)=>{r.r(t),r.d(t,{default:()=>n});var c=r(893),u=r(443);const n=function(){var e=(0,u.$)("about").t;return(0,c.jsx)("div",{children:e("О нас")})}}}]);
