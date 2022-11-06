1b3b603b03198e360b6a68d2f688c5584386afd3
Fin <fantomma91@gmail.com>
2022-11-06 19:31:54 +0700

    change screenshots, add concurrently

--- src/shared/config/storybook/decorators/StylesDecorator.tsx
+++ src/shared/config/storybook/decorators/StylesDecorator.tsx
@@ -1,4 +1,8 @@
 import 'app/styles/index.scss';
 import { Story } from '@storybook/react';
 
-export const StylesDecorator = (StoryComponent: Story) => <StoryComponent />;
+export const StylesDecorator = (StoryComponent: Story) => (
+    <div id="#loki">
+        <StoryComponent />
+    </div>
+);
