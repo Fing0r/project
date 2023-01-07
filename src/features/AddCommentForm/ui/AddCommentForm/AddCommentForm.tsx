080a87327d8fe3ea287f9ca561614684717ecc6a
Fin <fantomma91@gmail.com>
2023-01-07 21:22:00 +0700

    test lint-staged

--- src/features/AddCommentForm/ui/AddCommentForm/AddCommentForm.tsx
+++ src/features/AddCommentForm/ui/AddCommentForm/AddCommentForm.tsx
@@ -33,8 +33,8 @@ const AddCommentForm = memo((props: AddCommentFormProps) => {
     useDynamicModule(initialReducers);
 
     const { setText } = useAddCommentFormActions();
-    const error = useCommentFormError();
     const text = useCommentFormText();
+    const error = useCommentFormError();
 
     const onChangeCommentText = useCallback((value: string) => {
         setText(value);
