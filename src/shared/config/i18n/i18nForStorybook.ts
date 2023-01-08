68620ed5b3f7d6e6f5a361f63f89aa61090f80fd
Fin <fantomma91@gmail.com>
2023-01-08 15:15:56 +0700

    change i18n

--- src/shared/config/i18n/i18nForStorybook.ts
+++ src/shared/config/i18n/i18nForStorybook.ts
@@ -1,37 +1,24 @@
-import path from 'path';
-
 import i18n from 'i18next';
 import LanguageDetector from 'i18next-browser-languagedetector';
 import Backend from 'i18next-http-backend';
 import { initReactI18next } from 'react-i18next';
-// import en from './locales/en/translation.json';
-// import ru from './locales/ru/translation.json';
-
-// export const resources = {
-//     en: { translation: en },
-//     ru: { translation: ru },
-// } as const;
-
-const loadPath = path.resolve(__dirname, '..', '..', '..', '..', 'public', 'locales');
 
 i18n
     .use(Backend)
     .use(LanguageDetector)
     .use(initReactI18next)
     .init({
-        // resources,
         fallbackLng: 'ru',
         lng: 'ru',
         debug: true,
         interpolation: {
             escapeValue: false, // not needed for react!!
         },
-        ns: 'ru',
         defaultNS: 'translation',
         lowerCaseLng: true,
         backend: {
-            // loadPath: `${loadPath}/{{lng}}/{{ns}}.json`,
-            addPath: `${loadPath}/{{lng}}/{{ns}}.json`,
+            loadPath: '/locales/{{lng}}/{{ns}}.json',
+            addPath: '/public/locales/{{lng}}/{{ns}}.json',
         },
     });
 
